4faa48c3137ef5f9b9b77b980839e92e7f5a18bf
Mischa Beumer <mischabeumer@Mischas-MacBook-Pro.local>
2017-04-10 20:41:33 -0400

    Debugged the initial commit

---
+++ Photorama/FlickrAPI.swift
@@ -0,0 +1,101 @@
+//
+//  FlickrAPI.swift
+//  Photorama
+//
+//  Created by Mischa Beumer on 4/10/17.
+//  Copyright © 2017 msbbeumer. All rights reserved.
+//
+
+import Foundation
+
+enum Method: String {
+  case interestingPhotos = "flickr.interestingness.getList"
+}
+
+enum FlickrError: Error {
+  case invalidJSONData
+}
+
+struct FlickrAPI {
+  
+  private static let baseURLString = "https://api.flickr.com/services/rest"
+  private static let apiKey = "a6d819499131071f158fd740860a5a88"
+  
+  private static let dateFormatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+    return formatter
+  }()
+  
+  static var interestingPhotosURL: URL {
+    return flickrURL(method: .interestingPhotos, parameters: ["extras": "url_h,date_taken"])
+  }
+  
+  private static func flickrURL(method: Method, parameters: [String:String]?) -> URL {
+    
+    var components = URLComponents(string: baseURLString)!
+    
+    var queryItems = [URLQueryItem]()
+    
+    let baseParameters = [
+      "method": method.rawValue,
+      "format": "json",
+      "nojsoncallback": "1",
+      "api_key": apiKey
+    ]
+    
+    for (key,value) in baseParameters {
+      let item = URLQueryItem(name: key, value: value)
+      queryItems.append(item)
+    }
+    
+    if let additionalParameters = parameters {
+      for (key, value) in additionalParameters {
+        let item = URLQueryItem(name: key, value: value)
+        queryItems.append(item)
+      }
+    }
+    components.queryItems = queryItems
+    
+    return components.url!
+  }
+  
+  private static func photo(fromJSON json: [String : Any]) -> Photo? {
+    guard let photoID = json["id"] as? String,
+      let title = json["title"] as? String,
+      let dateString = json["dateTaken"] as? String,
+      let photoURLString = json["URL_h"] as? String,
+      let url = URL(string: photoURLString),
+      let dateTaken = dateFormatter.date(from: dateString) else {
+        return nil
+    }
+    
+    return Photo(title: title, photoID: photoID, remoteURL: url, dateTaken: dateTaken)
+  }
+  
+  static func photos(fromJSON data: Data) -> PhotosResult {
+    do {
+      let jsonObject = try JSONSerialization.jsonObject(with: data, options: [])
+      
+      guard let jsonDictionary = jsonObject as? [AnyHashable:Any],
+        let photos = jsonDictionary["photos"] as? [String:Any],
+        let photosArray = photos["photo"] as? [[String:Any]] else {
+          return .failure(FlickrError.invalidJSONData)
+      }
+      
+      var finalPhotos = [Photo]()
+      for photoJSON in photosArray {
+        if let photo = photo(fromJSON: photoJSON) {
+          finalPhotos.append(photo)
+        }
+      }
+      
+      if finalPhotos.isEmpty && !photosArray.isEmpty {
+        return .failure(FlickrError.invalidJSONData)
+      }
+      return .success(finalPhotos)
+    } catch let error {
+      return .failure(error)
+    }
+  }
+}
---
+++ Photorama/Photo.swift
@@ -0,0 +1,24 @@
+//
+//  Photo.swift
+//  Photorama
+//
+//  Created by Mischa Beumer on 4/10/17.
+//  Copyright © 2017 msbbeumer. All rights reserved.
+//
+
+import Foundation
+
+class Photo {
+  
+  let title: String
+  let remoteURL: URL
+  let photoID: String
+  let dateTaken: Date
+  
+  init(title: String, photoID: String, remoteURL: URL, dateTaken: Date) {
+    self.title = title
+    self.photoID = photoID
+    self.remoteURL = remoteURL
+    self.dateTaken = dateTaken
+  }
+}
